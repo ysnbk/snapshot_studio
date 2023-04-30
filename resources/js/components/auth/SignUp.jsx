83a6258ad7dc9ecdb4f835edab6df4e8445ddde2
yassine <yassineyassinux16@gmail.com>
2023-04-30 11:00:20 +0200

    change images to brand in signup & signin

--- resources/js/components/auth/SignUp.jsx
+++ resources/js/components/auth/SignUp.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import Logo from '../images/logo.png';
+import Logo from '../brand/logo.png';
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom';
 import Modal from 'react-modal';
