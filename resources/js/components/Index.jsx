4f25bd9b061465c9f4271741c49bb3f347649ef6
yassine <yassineyassinux16@gmail.com>
2023-04-30 14:37:49 +0200

    add loading page

--- resources/js/components/Index.jsx
+++ resources/js/components/Index.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from './Navbar'
+import Loading from './Loading'
 
 
 const Index = () => {
+  const [isLoading , setIsLoading]=useState(true)
+  useEffect(() => {
+
+    setIsLoading(true)
+    setIsLoading(false)
+  }, []);
   return (
+    isLoading?<Loading/>:
     <div>
       <Navbar
             links={[<li><Link className='link' to='register'>Sign Up</Link></li>
---
+++ resources/js/components/Loading.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactLoading from "react-loading";
+import logo from './brand/logo.png'
+
+const Loading = () => {
+  return (
+    <div className='position-absolute top-50 start-50 translate-middle' >
+        <img src={logo} alt="" width={200}/>
+        <ReactLoading type="bubbles" color="#198dbe"
+        height={200} width={150} />
+    </div>
+  )
+}
+
+export default Loading
